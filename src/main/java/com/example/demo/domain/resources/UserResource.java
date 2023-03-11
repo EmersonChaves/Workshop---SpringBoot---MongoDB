7bd2a46eaf763ed56f2a9d20f0f639c44ea87889
Emerson <emersoncchaves1@gmail.com>
2023-03-10 22:56:04 -0300

    entity

---
+++ src/main/java/com/example/demo/domain/resources/UserResource.java
@@ -0,0 +1,29 @@
+package com.example.demo.domain.resources;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.domain.User;
+
+@RestController
+@RequestMapping(value ="/users")
+public class UserResource {
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<User>> findAll(){
+		User maria = new User("1", "Maria", "maria@gmail.com");
+		User maria2 = new User("1", "Maria", "maria@gmail.com");
+		List<User> list = new ArrayList<>();
+		list.addAll(Arrays.asList(maria, maria2));
+				
+		return ResponseEntity.ok().body(list);
+	}
+
+	
+}
