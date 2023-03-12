e97d6fcf81ac132586d65e6181b60b9970c415ed
Emerson <emersoncchaves1@gmail.com>
2023-03-12 10:31:59 -0300

    Novo

--- src/main/java/com/example/demo/domain/resources/UserResource.java
+++ src/main/java/com/example/demo/domain/resources/UserResource.java
@@ -1,29 +1,31 @@
 package com.example.demo.domain.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.domain.User;
+import com.example.demo.services.UserService;
 
 @RestController
 @RequestMapping(value ="/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	@RequestMapping(method=RequestMethod.GET)
 	public ResponseEntity<List<User>> findAll(){
-		User maria = new User("1", "Maria", "maria@gmail.com");
-		User maria2 = new User("1", "Maria", "maria@gmail.com");
-		List<User> list = new ArrayList<>();
-		list.addAll(Arrays.asList(maria, maria2));
-				
+		
+		List<User> list = service.findAll();			
 		return ResponseEntity.ok().body(list);
 	}
 
 	
 }
+
